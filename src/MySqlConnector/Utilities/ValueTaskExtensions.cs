db55cec39ed2a9988f4b8e43cb3609a2a3071598
GHMatti <33543464+GHMatti@users.noreply.github.com>
2018-03-20 14:33:50 +0100

    Revert change #ad6b37

--- src/MySqlConnector/Utilities/ValueTaskExtensions.cs
+++ src/MySqlConnector/Utilities/ValueTaskExtensions.cs
@@ -5,7 +5,11 @@ namespace MySqlConnector.Utilities
 {
 	internal static class ValueTaskExtensions
 	{
-		public static async ValueTask<TResult> ContinueWith<T, TResult>(this ValueTask<T> valueTask, Func<T, ValueTask<TResult>> continuation) => await continuation(await valueTask.ConfigureAwait(false)).ConfigureAwait(false);
+		public static ValueTask<TResult> ContinueWith<T, TResult>(this ValueTask<T> valueTask, Func<T, ValueTask<TResult>> continuation)
+		{
+			return valueTask.IsCompleted ? continuation(valueTask.Result) :
+				new ValueTask<TResult>(valueTask.AsTask().ContinueWith(task => continuation(task.GetAwaiter().GetResult()).AsTask()).Unwrap());
+		}
 
 		public static ValueTask<T> FromException<T>(Exception exception) => new ValueTask<T>(Utility.TaskFromException<T>(exception));
 	}
